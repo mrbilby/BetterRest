e7d2565bee879bef40844cd0c116a21781232a30
mrbilby <baileymrb@gmail.com>
2023-03-22 18:57:52 +0000

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by James Bailey on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
